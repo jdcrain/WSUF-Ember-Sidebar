6e88a9fdb398f0190be4b429fd28aa846c2e5d21
Jared Crain <jared.crain@wsu.edu>
2018-08-14 15:54:11 -0700

    Add a timeout for fetching the available applications

--- addon/components/wsuf-sidebar.js
+++ addon/components/wsuf-sidebar.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 import layout from '../templates/components/wsuf-sidebar';
 import fetch from 'fetch';
-import { task, timeout } from 'ember-concurrency';
+import { task, timeout, race } from 'ember-concurrency';
 
 export default Component.extend({
   layout,
@@ -12,27 +12,39 @@ export default Component.extend({
   tabindex: "0",
 
   didUpdateAttrs() {
-      this._super(...arguments);
-      let userId = this.get('userId');
-      if (userId) {
-          this.get('getApps').perform(userId);
-      }
+    this._super(...arguments);
+    let userId = this.get('userId');
+    if (userId) {
+        this.get('getApps').perform(userId);
+    }
   },
 
   getApps: task(function * (id) {
-      let url = this.get('APIUrl') + '/oktauser/' + id + '/apps';
-      const context = this;
+    let url = `${this.get('APIUrl')}/oktauser/${id}/apps`;
+    const context = this;
 
-      let resp = yield fetch(url);
-      let apps = yield resp.json();
+    let resp = this.get('fetchApps').perform(url);
+    yield race([resp, timeout(30000)]);
+    
+    if (resp.isSuccessful) {
+        let apps = yield resp.json();
 
-      let appList = [];
-      
-      for (let i = 0; i < apps.length; i++) {
-          appList.addObject(apps[i]);
-          context.set('apps', appList);
-          yield timeout(100);
-      }
+        let appList = [];
+
+        for (let i = 0; i < apps.length; i++) {
+            appList.addObject(apps[i]);
+            context.set('apps', appList);
+            yield timeout(100);
+        }
+    }
+    else {
+        resp.cancel();
+        this.set('apps', []);
+    }
 
   }).drop(),
+
+  fetchApps: task(function * (url) {
+    return fetch(url);
+  }).drop()
 });
