be3df45103ec27a480ad61076ba5ee783dcf8d38
Jared Crain <jared.crain@wsu.edu>
2018-08-15 10:48:48 -0700

    Adjust styling of logout button

--- addon/components/log-out.js
+++ addon/components/log-out.js
@@ -3,6 +3,7 @@ import layout from '../templates/components/log-out';
 
 export default Component.extend({
   layout,
+  classNames: ["logout"],
   actions: {
     logoff() {
       let widget = this.get('widget');
